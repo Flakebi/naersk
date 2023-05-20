4910b01b901d5d555e9b1aa3501873972a6f6b66
Sebastian Neubauer <Sebastian.Neubauer@amd.com>
2023-05-20 13:41:59 +0200

    Fix

--- examples/cross-windows/flake.nix
+++ examples/cross-windows/flake.nix
@@ -48,9 +48,13 @@
           # (https://doc.rust-lang.org/cargo/reference/config.html#buildtarget)
           CARGO_BUILD_TARGET = "x86_64-pc-windows-gnu";
 
+          # Fix: some `extern` functions couldn't be found; some native libraries may need to be installed or have their path specified
+          # when using C dependencies
+          CC_x86_64_pc_windows_gnu = "x86_64-w64-mingw32-gcc";
+
           # Tells Cargo that it should use Wine to run tests.
           # (https://doc.rust-lang.org/cargo/reference/config.html#targettriplerunner)
-          CARGO_TARGET_X86_64_PC_WINDOWS_GNU_RUNNER = pkgs.writeScript "wine-wrapper" ''
+          CARGO_TARGET_X86_64_PC_WINDOWS_GNU_RUNNER = pkgs.writeShellScript "wine-wrapper" ''
             export WINEPREFIX="$(mktemp -d)"
             exec wine64 $@
           '';
